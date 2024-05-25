de462c8477a78d6895937572dd69f5fe1cfb9299
Roland <hollywood2115@gmail.com>
2024-05-25 11:16:45 -0500

    updated api routes.

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -5,4 +5,31 @@ const { CustomErrHandler } = require('../../errors/error');
 
 const router = express.Router();
 
+//READ - reviews by current user
+router.get('/current', async(req, res, next)=>{
+    try {
+        res.json({message: "reviews current"})
+    } catch (err) {
+        
+    }
+})
+
+//UPDATE
+router.put('/:reviewId', async(req, res, next)=>{
+    try {
+        res.json({message: "review update"})
+    } catch (err) {
+        
+    }
+})
+
+//DELETE
+router.delete('/:reviewId', async(req, res, next)=>{
+    try {
+        res.json({message: "delete review"})
+    } catch (err) {
+        
+    }
+})
+
 module.exports = router
