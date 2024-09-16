c596cec2b6e6fa9b043047a0d7c248275bf1c10e
Roland <hollywood2115@gmail.com>
2024-09-15 21:00:55 -0500

    still trying

--- frontend/src/components/Spots/Spots.jsx
+++ frontend/src/components/Spots/Spots.jsx
@@ -3,14 +3,14 @@ import * as spotActions from '../../store/spots';
 import { useDispatch, useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import "./Spots.css"
-import { AiFillStar } from 'react-icons/ai';
+// import { AiFillStar } from 'react-icons/ai';
 
 
 function Spots() {
   const dispatch = useDispatch();
   const spots = useSelector(state => state.spotsReducer.spots)
   const [isLoaded, setIsLoaded] = useState(false);
-  const star = AiFillStar()
+  // const star = AiFillStar()
 
   useEffect(() => {
       dispatch(spotActions.getAllSpots())
@@ -18,7 +18,7 @@ function Spots() {
         setIsLoaded(true)})
   }, [dispatch])
 
-// let findSpots = ()
+
   
   return (
     <>
@@ -26,9 +26,9 @@ function Spots() {
       {spots && isLoaded && spots.map(spot => (
   <div key={spot.id} className="spotContainer" >
             <NavLink to={`/spots/${spot.id}`} id={spot.id}>
-            <span className="tooltiptext">{spot.name}</span>
+            {/* <span className="tooltiptext">{spot.name}</span>
               <img src={`${spot.previewImage.url}`}></img>
-              <p>{spot.city}, {spot.state} {star}{Math.round(spot.avgRating * 100)/100} <br></br>{spot.price} per night</p>
+              <p>{spot.city}, {spot.state} {star}{Math.round(spot.avgRating * 100)/100} <br></br>{spot.price} per night</p> */}
             </NavLink>
           </div>
         ))}
