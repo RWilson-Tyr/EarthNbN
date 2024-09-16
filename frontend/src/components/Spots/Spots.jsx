f256fa4ee22c73e8d45b6f70f1378db0286d2e68
Roland <hollywood2115@gmail.com>
2024-09-15 21:50:35 -0500

    all one image

--- frontend/src/components/Spots/Spots.jsx
+++ frontend/src/components/Spots/Spots.jsx
@@ -1,16 +1,16 @@
 import { useEffect, useState } from 'react';
 import * as spotActions from '../../store/spots';
 import { useDispatch, useSelector } from 'react-redux';
-// import { NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import "./Spots.css"
-// import { AiFillStar } from 'react-icons/ai';
+import { AiFillStar } from 'react-icons/ai';
 
 
 function Spots() {
   const dispatch = useDispatch();
-  const spots = useSelector(state => state.spotsReducer)
+  const spots = useSelector(state => state.spotsReducer.spots)
   const [isLoaded, setIsLoaded] = useState(false);
-  // const star = AiFillStar()
+  const star = AiFillStar()
 
   useEffect(() => {
       dispatch(spotActions.getAllSpots())
@@ -18,24 +18,22 @@ function Spots() {
         setIsLoaded(true)})
   }, [dispatch])
 
-  console.log("SPOTS",spots)
-
-  console.log("MAP", spots.spots.map((spot)=>console.log("EACH SPOT", spot)))
+  console.log(spots)
 
 
-  {isLoaded}
+  
   return (
     <>
       <div className="spots">
-      {/* {spots && isLoaded && spots.map(spot => ( */}
-   {/* <div key={spot.id} className="spotContainer" > */}
-            {/* <NavLink to={`/spots/${spot.id}`} id={spot.id}> */}
-            {/* <span className="tooltiptext">{spot.name}</span> */}
-              {/* <img src={`${spot.previewImage.url}`}></img> */}
-              {/* <p>{spot.city}, {spot.state} {star}{Math.round(spot.avgRating * 100)/100} <br></br>{spot.price} per night</p> */}
-            {/* </NavLink> */}
-          {/* </div> */}
-        {/* ))} */}
+      {spots && isLoaded && spots.map(spot => (
+  <div key={spot.id} className="spotContainer" >
+            <NavLink to={`/spots/${spot.id}`} id={spot.id}>
+            <span className="tooltiptext">{spot.name}</span>
+              <img src='https://www.houseplans.net/news/wp-content/uploads/2023/07/57260-768.jpeg'></img>
+              <p>{spot.city}, {spot.state} {star}{Math.round(spot.avgRating * 100)/100} <br></br>{spot.price} per night</p>
+            </NavLink>
+          </div>
+        ))}
       </div>
     </>
   );
